d07de8f0780cd21b4acf025aa15c898c97be7ef2
hiddenviki <Veselkova.v@yandex.ru>
2024-05-13 23:06:18 +0300

    #feature/2 *Первые маршруты диалога

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\DialogController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('dialog/create', [DialogController::class, 'create']);
+Route::get('dialog/landlord_tenant/get', [DialogController::class, 'getByLandlordAndTenantIds']);
